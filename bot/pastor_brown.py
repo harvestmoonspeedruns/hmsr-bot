5b6237b839c322988131f72016b3c599a4269bba
Blake Grotewold <blake.grotewold@gmail.com>
2018-12-27 12:10:05 -0600

    Add Licensing to files

--- bot/pastor_brown.py
+++ bot/pastor_brown.py
@@ -1,3 +1,13 @@
+'''
+Copyright 2019 Blake Grotewold <hello@grote.world>
+
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+'''
+
 from bot.twitch import TwitchBot
 
 class PastorBrown(TwitchBot):
@@ -39,4 +49,4 @@ class PastorBrown(TwitchBot):
             elif len(winners) == 1:
                 congrats_to = "you"
 
-            self.send("Winners are {0}. Congrats to {1}!".format(" ".join(winners), congrats_to))
+            self.send("Winners are {0}. Congrats to {1}!".format(" @".join(winners), congrats_to))
