32d996ca80bf4c4ff7a832232d14b0eefd4d3a21
Blake Grotewold <blake.grotewold@gmail.com>
2018-12-27 15:50:46 -0600

    Add Spam Protection: 10 seconds per command per printout

--- bot/pastor_brown.py
+++ bot/pastor_brown.py
@@ -8,6 +8,7 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 """
 
+import datetime
 from bot.twitch import TwitchBot
 from bot.command_list import CommandList
 
@@ -16,9 +17,11 @@ class PastorBrown(TwitchBot):
     def __init__(self, username, client_id, token, channel):
         super().__init__(username, client_id, token, channel)
         self.command_list = CommandList()
+        self.previousCommands = {}
 
     def do_command(self, e, cmd):
-        if cmd in self.command_list.commands.keys():
+        if cmd in self.command_list.commands.keys() and (cmd not in self.previousCommands or datetime.datetime.now() > self.previousCommands[cmd]):
             message = self.command_list.commands[cmd](e, self.channel)
             if message:
+                self.previousCommands[cmd] = datetime.datetime.now() + datetime.timedelta(0,10)
                 self.send(message)
