0127831e2f2b05e3a0c8614749017dfbe2e616a4
Blake Grotewold <blake.grotewold@gmail.com>
2018-12-27 14:49:44 -0600

    add All Photos Info to bot

--- bot/command_list.py
+++ bot/command_list.py
@@ -1,9 +1,9 @@
 from bot.commands.hm64_race_commands import HM64RaceCommands
+from bot.commands.hm64_all_photos_info import HM64AllPhotosInfo
 
 
 class CommandList(object):
-    _command_cls = [HM64RaceCommands]
-
+    _command_cls = [HM64RaceCommands, HM64AllPhotosInfo]
 
     def __init__(self):
         self.commands = {}
