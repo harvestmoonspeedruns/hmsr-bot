1b11af46834825d8b180f70de3afbc983e7e5f95
Blake Grotewold <hello@grote.world>
2020-08-05 19:47:39 -0500

    auth checking for find game ids

--- actions/find_game_id.js
+++ actions/find_game_id.js
@@ -5,7 +5,19 @@ const args = process.argv.slice(2);
 
 if (args.length < 1) process.exit(1); // Don't go further
 
-const options = { headers: { 'Client-Id': process.env.TWITCH_CLIENT_ID } };
-fetch(`https://api.twitch.tv/helix/games?name=${args[0]}`, options)
-  .then(res => res.json())
-  .then(game => console.log(game));
+const authOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+};
+const authQueryString = `?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials`;
+const twitchAuthEndpoint = `https://id.twitch.tv/oauth2/token${authQueryString}`;
+
+fetch(twitchAuthEndpoint, authOptions).then(res => res.json()).then((auth) => {
+  const options = { headers: { Authorization: `Bearer ${auth.access_token}`, 'Client-Id': process.env.TWITCH_CLIENT_ID } };
+
+  fetch(`https://api.twitch.tv/helix/games?name=${args[0]}`, options)
+    .then(res => res.json())
+    .then(game => console.log(game));
+});
